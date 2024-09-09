61434405b2801540fbeb2ae9fd92c24e4356b9bb
OscsRH <oscarromeroh27@gmail.com>
2024-09-09 14:50:47 -0600

    finished API with NestJS tutorial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+//import { MyLoggerService } from './my-logger/my-logger.service';
+import { AllExeceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule /*, {
+    bufferLogs: true,
+  }*/,
+  );
+  //app.useLogger(app.get(MyLoggerService));
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExeceptionsFilter(httpAdapter));
+  app.enableCors();
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
